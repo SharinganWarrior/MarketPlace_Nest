e86f98db39f0349a4ead514b79e5019b5b1dfbdb
SharinganWarrior <31237825+SharinganWarrior@users.noreply.github.com>
2023-06-06 11:12:35 +0500

    init
    init

---
+++ BackEnd/src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from "typeorm";
+import { User } from './entities/user.entity';
+import { InjectRepository } from "@nestjs/typeorm"
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+){}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    return this.userRepository.save(createUserDto);
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
